f7935b39becb5200728edbe26ed82ac0409e5449
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2016-03-27 17:03:23 +0000

    Added a thread in start function, and implemented init function.

--- KiloboltGame/src/kiloboltgame/StartingClass.java
+++ KiloboltGame/src/kiloboltgame/StartingClass.java
@@ -1,5 +1,48 @@
 package kiloboltgame;
 
-public class StartingClass {
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
 
+public class StartingClass extends Applet implements Runnable {
+
+	@Override
+	public void init() {
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+	}
+
+	@Override
+	public void start() {
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }

e23931e5a4effc0f3e2f6a13743a8904ae0bbac6
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2016-03-27 17:18:27 +0000

    Added KeyListener for user input.

--- KiloboltGame/src/kiloboltgame/StartingClass.java
+++ KiloboltGame/src/kiloboltgame/StartingClass.java
@@ -3,14 +3,17 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	@Override
 	public void init() {
 		setSize(800, 480);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-Bot Alpha");
 	}
@@ -43,6 +46,53 @@ public class StartingClass extends Applet implements Runnable {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		   case KeyEvent.VK_UP:
+		   break;
+
+		   case KeyEvent.VK_DOWN:
+		   break;
+
+		   case KeyEvent.VK_LEFT:
+		   break;
+
+		   case KeyEvent.VK_RIGHT:
+		   break;
+
+		   case KeyEvent.VK_SPACE:
+		   break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		   case KeyEvent.VK_UP:
+		      break;
+
+		   case KeyEvent.VK_DOWN:
+		      break;
+
+		   case KeyEvent.VK_LEFT:
+		      break;
+
+		   case KeyEvent.VK_RIGHT:
+		      break;
+		      
+		   case KeyEvent.VK_SPACE:
+		      break;
 		}
 	}
 }
